72b18a711acce6a1ad18dc9132601e6e7a2d64f2
KattyFury <kattyfury1403@gmail.com>
2026-06-25 18:12:21 +0700

    Google login: use SocialLoginProvider.GOOGLE enum instead of string

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -2,7 +2,7 @@
 import emailIcon from '../../icon/email.png'
 import googleIcon from '../../icon/google.png'
 import { useNav } from '../nav'
-import { W3SSdk } from '@circle-fin/w3s-pw-web-sdk'
+import { W3SSdk, SocialLoginProvider } from '@circle-fin/w3s-pw-web-sdk'
 import { createSocialToken, initializeWallet, executeChallenge, getWalletAddress, GOOGLE_CLIENT_ID } from '../circle'
 
 const APP_ID = '518fec6a-4680-5175-9de6-0810fb3dfd04'
@@ -60,7 +60,7 @@ export default function Login() {
         }
       )
 
-      googleSdk.performLogin('google')
+      googleSdk.performLogin(SocialLoginProvider.GOOGLE)
     } catch (e) {
       console.error('Google login error:', e)
     }
